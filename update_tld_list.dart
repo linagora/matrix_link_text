6f60e56cf9234e0b720c281cf021e7e8377ab59b
Christian Pauly <christian-pauly@posteo.de>
2021-04-02 08:38:01 +0200

    fix: Null safe dependencies

--- update_tld_list.dart
+++ update_tld_list.dart
@@ -8,7 +8,7 @@ const ICAAN_URL = 'https://data.iana.org/TLD/tlds-alpha-by-domain.txt';
 const OUT_FILE = './lib/tlds.dart';
 
 void main() async {
-  final res = utf8.decode((await http.get(ICAAN_URL)).bodyBytes);
+  final res = utf8.decode((await http.get(Uri.parse(ICAAN_URL))).bodyBytes);
   final file = await File(OUT_FILE).open(mode: FileMode.write);
   await file.writeString('const ALL_TLDS = {\n');
   for (var tld in res.split('\n')) {
